d497c5af67f47ae84186672a01565d5cd9b66799
keshew <artyom.korotckov2017@yandex.ru>
2025-10-29 18:49:04 +0500

    Initial Commit

---
+++ ChikenIncubator/ChikenIncubatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChikenIncubatorApp.swift
+//  ChikenIncubator
+//
+//  Created by Артём Коротков on 29.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChikenIncubatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
